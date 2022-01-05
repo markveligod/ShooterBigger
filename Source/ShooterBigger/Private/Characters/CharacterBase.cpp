d03b6a210aebba545d19a3cf94fbf870c36f26c4
Mark <34418187+markveligod@users.noreply.github.com>
2022-01-05 16:50:07 +0300

    add assets in content browser

---
+++ Source/ShooterBigger/Private/Characters/CharacterBase.cpp
@@ -0,0 +1,28 @@
+// Copyright 2021, Infima Games. All Rights Reserved.
+
+#include "Characters/CharacterBase.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ACharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
