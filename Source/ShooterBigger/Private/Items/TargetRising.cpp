c29fd69533420660d60c2d13ad323ec625968ea8
Mark <34418187+markveligod@users.noreply.github.com>
2022-01-14 13:16:43 +0300

    add hit effect for target class and refactor folder in content

--- Source/ShooterBigger/Private/Items/TargetRising.cpp
+++ Source/ShooterBigger/Private/Items/TargetRising.cpp
@@ -1,21 +1,59 @@
 
 #include "Items/TargetRising.h"
+#include "Sound/SoundCue.h"
+#include "Kismet/GameplayStatics.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogTargetRising, All, All);
 
 // Sets default values
 ATargetRising::ATargetRising()
 {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	this->Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(this->Root);
+
+	this->MeshTarget = CreateDefaultSubobject<USkeletalMeshComponent>("Skeletal Mesh target");
+	this->MeshTarget->SetupAttachment(this->Root);
 }
 
 // Called when the game starts or when spawned
 void ATargetRising::BeginPlay()
 {
 	Super::BeginPlay();
+
+	OnTakePointDamage.AddDynamic(this, &ATargetRising::RegisterPointTakeDamage);
+	this->StateTarget = EStateTarget::Ready;
 }
 
-// Called every frame
-void ATargetRising::Tick(float DeltaTime)
+void ATargetRising::RegisterPointTakeDamage(AActor* DamagedActor, float Damage, AController* InstigatedBy, FVector HitLocation,
+	UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const UDamageType* DamageType, AActor* DamageCauser)
 {
-	Super::Tick(DeltaTime);
+	if (this->StateTarget != EStateTarget::Ready) return;
+
+	this->StateTarget = EStateTarget::Reload;
+	this->MeshTarget->PlayAnimation(this->AnimTargetDown, false);
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), this->SoundTargetDown, GetActorLocation(), FRotator::ZeroRotator);
+
+	FTimerHandle TimerHandle;
+	GetWorldTimerManager().SetTimer(
+		TimerHandle, this, &ATargetRising::ResetReloadTarget, FMath::RandRange(this->RangeReload.Min, this->RangeReload.Max), false);
+
+	const FString NameDamagedActor = (DamagedActor) ? DamagedActor->GetName() : "NULL";
+	const FString NameDamageType = (DamageType) ? DamageType->GetName() : "NULL";
+	const FString NameInstigatedBy = (InstigatedBy) ? InstigatedBy->GetName() : "NULL";
+	const FString NameDamageCauser = (DamageCauser) ? DamageCauser->GetName() : "NULL";
+
+	UE_LOG(LogTargetRising, Display,
+		TEXT("Name damage actor: %s | Amount damage: %f | Hit Location: %s | Name damage type: %s | Instigated controller: %s | Damage "
+			 "Causer : %s"),
+		*NameDamagedActor, Damage, *HitLocation.ToString(), *NameDamageType, *NameInstigatedBy, *NameDamageCauser);
+}
+
+void ATargetRising::ResetReloadTarget()
+{
+	this->MeshTarget->PlayAnimation(this->AnimTargetUp, false);
+	UGameplayStatics::PlaySoundAtLocation(GetWorld(), this->SoundTargetUp, GetActorLocation(), FRotator::ZeroRotator);
+	this->StateTarget = EStateTarget::Ready;
 }
--- Source/ShooterBigger/Public/Items/TargetRising.h
+++ Source/ShooterBigger/Public/Items/TargetRising.h
@@ -6,6 +6,16 @@
 #include "GameFramework/Actor.h"
 #include "TargetRising.generated.h"
 
+class USoundCue;
+UENUM()
+enum class EStateTarget
+{
+	None = 0,
+	Ready,
+	Reload,
+	Destroy
+};
+
 UCLASS()
 class SHOOTERBIGGER_API ATargetRising : public AActor
 {
@@ -19,7 +29,43 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+private:
+	// root component
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USceneComponent* Root;
+
+	// Base skeletal mesh for target
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USkeletalMeshComponent* MeshTarget;
+
+	// Range time in sec for Reload state
+	UPROPERTY(EditDefaultsOnly, Category = "Settings|Hit")
+	FFloatInterval RangeReload = {3.0f, 6.0f};
+
+	// Animation from hitting the target
+	UPROPERTY(EditDefaultsOnly, Category = "Settings|Hit|Anim")
+	UAnimationAsset* AnimTargetDown;
+
+	// Animation from returning to the starting position
+	UPROPERTY(EditDefaultsOnly, Category = "Settings|Hit|Anim")
+	UAnimationAsset* AnimTargetUp;
+
+	// Sound from hitting the target
+	UPROPERTY(EditDefaultsOnly, Category = "Settings|Hit|Sound")
+	USoundCue* SoundTargetDown;
+
+	// Sound from returning to the starting position
+	UPROPERTY(EditDefaultsOnly, Category = "Settings|Hit|Sound")
+	USoundCue* SoundTargetUp;
+
+	// Registering the damage received for the current Target
+	UFUNCTION()
+	void RegisterPointTakeDamage(AActor* DamagedActor, float Damage, AController* InstigatedBy, FVector HitLocation,
+		UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const UDamageType* DamageType, AActor* DamageCauser);
+
+	// Reset reload to ready
+	void ResetReloadTarget();
+
+	// Current state target rising
+	EStateTarget StateTarget = EStateTarget::None;
 };
